81803b64fac2e02198d90001f8e5322a0b071024
Lenny Martinez Dominguez <lennymartinezd@gmail.com>
2020-10-07 21:57:11 -0400

    add stuff

--- redditScraping/psaw.py
+++ redditScraping/psaw.py
@@ -7,21 +7,22 @@ from psaw import PushshiftAPI
 
 api = PushshiftAPI()
 
+df = pd.DataFrame(
+    list(
+        api.search_submissions(
+            subreddit="COVID19_support",
+            filter=["title", "url", "selftext", "score"],
+            limit=60000,
+        )
+    )
+)
 
-
-
-df = pd.DataFrame(list(api.search_submissions(
-                            subreddit='COVID19_support',
-                            filter=['title', 'url', 'selftext', 'score'],
-                            limit=60000)))
-
-
-
-df['created_utc'] = pd.to_datetime(df['created_utc'],unit='s').dt.date
+# df["created_utc"] = pd.to_datetime(df["created_utc"], unit="s").dt.date
 
 
 print(df.head())
 print(len(df))
 
 
-df.to_csv('dataset_1006.csv')
+# df.to_csv("dataset_1006.csv")
+
